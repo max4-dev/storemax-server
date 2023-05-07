e97906bebde2ae582359a54798d8ff1819b3910e
Max <maksim.bogomyakov@gmail.com>
2023-05-07 14:46:17 +0300

    added uploading images in server

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import fs from 'fs';
 import mongoose from 'mongoose';
 import multer from 'multer';
 import cors from 'cors';
+import B2 from 'backblaze-b2';
 
-import { registerValidation, loginValidation, goodsValidation } from './validations.js';
+import { registerValidation, loginValidation } from './validations.js';
 import { UserController, GoodsController } from './controllers/index.js';
 import { chechAuth, checkIsAdmin, handleValidationErrors } from './utils/index.js';
 
@@ -18,6 +19,29 @@ mongoose
   });
 
 const app = express();
+
+const b2 = new B2({
+  applicationKeyId: process.env.B2_KEY_ID,
+  applicationKey: process.env.B2_KEY,
+});
+
+b2.authorize().then(() => {
+  console.log('B2 authorized');
+});
+
+const uploadFileToB2 = async (bucketName, fileName, filePath) => {
+  const bucket = await b2.getBucket({ bucketName });
+  const uploadUrl = await b2.getUploadUrl({ bucketId: process.env.BUCKEET_ID });
+  const fileData = await fs.promises.readFile(filePath);
+  const fileInfo = await b2.uploadFile({
+    uploadUrl: uploadUrl.data.uploadUrl,
+    uploadAuthToken: uploadUrl.data.authorizationToken,
+    bucketId: process.env.BUCKEET_ID,
+    fileName: fileName,
+    data: fileData,
+  });
+  return fileInfo;
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -42,10 +66,21 @@ app.post('/auth/register', registerValidation, handleValidationErrors, UserContr
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.get('/auth/me', chechAuth, UserController.getMe);
 
-app.post('/upload', checkIsAdmin, upload.single('image'), (req, res) => {
-  res.json({
-    url: `/uploads/${req.file.filename}`,
-  });
+app.post('/upload', checkIsAdmin, upload.single('image'), async (req, res) => {
+  const fileName = req.file.filename;
+  const filePath = req.file.path;
+  const bucketName = process.env.BUCKET_NAME;
+
+  try {
+    const fileInfo = await uploadFileToB2(bucketName, fileName, filePath);
+    const fileUrl = `https://f001.backblazeb2.com/file/${bucketName}/${fileInfo.data.fileName}`;
+    res.json({ url: fileUrl });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Failed to upload file' });
+  } finally {
+    fs.unlinkSync(filePath);
+  }
 });
 
 app.get('/goods', GoodsController.getAll);
